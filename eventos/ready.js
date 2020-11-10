e9366c8672caa925f7760bfb575dd8e6b31fe9fb
yFake520 <74261471+yFake520@users.noreply.github.com>
2020-11-10 14:42:14 -0300

    Delete ready.js

--- eventos/ready.js
+++
@@ -1,8 +0,0 @@
-const {bot} = require('../index');
-
-bot.on("ready", async () => {
-    console.log(`${bot.user.username} (${bot.user.id}) está online.`)
-
-    await bot.user.setActivity("a trabalhar!")
-    bot.user.setActivity('SkyIce!')
-})
